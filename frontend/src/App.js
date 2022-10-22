018b745ac266cf2c0da84005d78a2ad7c21f750b
Justin Welsh <justinwelsh33@gmail.com>
2022-10-22 01:02:13 -0500

    added header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import PokeCard from "./components/PokeCard";
 import { useState, useEffect } from 'react';
+import Header from "./components/Header";
 
 
 function App() {
@@ -21,14 +22,14 @@ function App() {
 
 
     return (
-    <div className="container mx-auto">
-        <h1 className="text-3xl font-bold text-center">
-        Pokedex.
-        </h1>
-        <div className="flex flex-wrap justify-center gap-7">
-            {pokemonGrid}
-        </div>
-    </div>
+        <>
+            <Header />
+            <div className="container mx-auto">
+                <div className="flex flex-wrap justify-center gap-7">
+                    {pokemonGrid}
+                </div>
+            </div>
+        </>
   );
 }
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+
+    return (
+        <div className="navbar bg-base-100 text-primary-content mb-10 flex justify-center">
+            <h1 className="text-3xl font-bold">
+                Pokedex.
+            </h1>
+        </div>
+    )
+}
+export default Header
