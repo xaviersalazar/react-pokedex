98df921f17d8e23a70e144a47f668ad1423b4ad6
Justin Welsh <justinwelsh33@gmail.com>
2022-10-22 22:32:41 -0500

    style update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="navbar bg-base-100 text-primary-content mb-10 flex justify-center">
             <h1 className="text-3xl font-bold">
-                Pokedex.
+                Pokedex<span className="text-secondary">.</span>
             </h1>
         </div>
     )
