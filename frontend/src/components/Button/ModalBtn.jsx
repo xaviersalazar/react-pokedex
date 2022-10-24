a232578b638f3cd3868314ee6b38c915ba859d8b
Justin Welsh <justinwelsh33@gmail.com>
2022-10-24 17:14:07 -0500

    added state to conditionally render card to limit API calls

--- frontend/src/components/Button/ModalBtn.jsx
+++ frontend/src/components/Button/ModalBtn.jsx
@@ -1,14 +1,19 @@
 import ViewMoreCard from "../Card/ViewMoreCard";
+import {useState} from "react";
 
 const ModalBtn = (props) => {
+
+    const [modalClicked, setModalClicked] = useState(false)
+    const toggleModalClicked = () => setModalClicked(prevState => !prevState)
+
     return (
         <>
-            <label htmlFor={props.id} className="btn modal-button btn-primary">View More</label>
+            <label htmlFor={props.id} className="btn modal-button btn-primary" onClick={toggleModalClicked}>View More</label>
 
             <input type="checkbox" id={props.id} className="modal-toggle" />
             <div className="modal modal-bottom sm:modal-middle">
                 <div className="modal-box">
-                    <ViewMoreCard pokemon={props.pokemon}/>
+                    {modalClicked && <ViewMoreCard pokemon={props.pokemon}/>}
                     <div className="modal-action">
                         <label htmlFor={props.id} className="btn">Close</label>
                     </div>
