48129dc08abf4fe8e8c8da2fff4e79eb3ad7561b
Justin Welsh <justinwelsh33@gmail.com>
2022-10-24 17:28:18 -0500

    added state to conditionally render card to limit API calls

--- frontend/src/components/Card/PokeCard.jsx
+++ frontend/src/components/Card/PokeCard.jsx
@@ -22,24 +22,26 @@ const PokeCard = (props) => {
     }
 
     return (
-        <div className="card w-80 bg-base-100 shadow-xl border-8 border-amber-200">
+        pokemon && (
+            <div className="card w-80 bg-base-100 shadow-xl border-8 border-amber-200">
 
-            <div className="card-body">
-                <div className="flex justify-between items-center">
-                    <h2 className="card-title">{props.name}</h2>
-                    <div className="badge badge-secondary">{getType()}</div>
-                </div>
-                <figure className="border-2 rounded-md bg-gradient-to-r from-white via-blue-250 to-blue-500">
-                    <img src={pokemon?.sprites.front_default} alt={props.name}  />
-                </figure>
+                <div className="card-body">
+                    <div className="flex justify-between items-center">
+                        <h2 className="card-title">{props.name}</h2>
+                        <div className="badge badge-secondary">{getType()}</div>
+                    </div>
+                    <figure className="border-2 rounded-md bg-gradient-to-r from-white via-blue-250 to-blue-500">
+                        <img src={pokemon.sprites.front_default} alt={props.name}  />
+                    </figure>
 
-                {getStats()}
+                    {getStats()}
 
-                <div className="card-actions justify-end">
-                    <ModalBtn id={pokemon?.id} pokemon={pokemon} />
+                    <div className="card-actions justify-end">
+                        <ModalBtn id={pokemon.id} pokemon={pokemon} />
+                    </div>
                 </div>
             </div>
-        </div>
+        )
     )
 }
 
