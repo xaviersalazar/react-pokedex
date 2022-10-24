8f132625291543ee86dc46a68a8c1b8f209a13ab
Justin Welsh <justinwelsh33@gmail.com>
2022-10-24 17:29:17 -0500

    Species API call to render "habitat"

--- frontend/src/components/Card/ViewMoreCard.jsx
+++ frontend/src/components/Card/ViewMoreCard.jsx
@@ -1,16 +1,30 @@
+import {useEffect, useState} from "react";
+
 const ViewMoreCard = (props) => {
 
+    const [species, setSpecies] = useState(null)
+
+    useEffect( () => {
+        fetch(props.pokemon.species.url)
+            .then(res => res.json())
+            .then(data => setSpecies(data))
+    }, [])
+
+    console.log(species);
     return (
         <>
-            <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
-            <p className="py-4">Welcome to the modal that will display the details below about the Pokemon you chose!</p>
-            <ul>
-                <li>Abilities</li>
-                <li>All Evolutions</li>
-                <li>Habitat</li>
-                <li>Height - {props.pokemon?.height}</li>
-                <li>Weight - {props.pokemon?.weight}</li>
-            </ul>
+            {species &&
+                <div>
+                    <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
+                    <p className="py-4">Welcome to the modal that will display the details below about the Pokemon you chose!</p>
+                    <ul>
+                        <li>Abilities</li>
+                        <li>All Evolutions</li>
+                        <li>Habitat - {species.habitat.name}</li>
+                        <li>Height - {props.pokemon?.height}</li>
+                        <li>Weight - {props.pokemon?.weight}</li>
+                    </ul>
+                </div>}
         </>
     )
 }
