6878c048d425397a83960ba7c008623346f18e3e
Justin Welsh <justinwelsh33@gmail.com>
2022-10-25 01:54:13 -0500

    update pagination

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,29 +7,23 @@ import PaginationBtn from "./components/Button/PaginationBtn";
 function App() {
 
     const [pokemonData, setPokemonData] = useState(null)
-    const [limitPage, setLimitPage] = useState(30)
     const [offsetPage, setOffsetPage] = useState(0)
 
 
     useEffect(() => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${limitPage}&offset=${offsetPage}`)
+        fetch(`https://pokeapi.co/api/v2/pokemon/?limit=30&offset=${offsetPage}`)
             .then(res => res.json())
             .then(data => setPokemonData(prevState => data))
-    }, [limitPage, offsetPage])
+    }, [offsetPage])
 
     const pokemonGrid = pokemonData?.results?.map(poke => <PokeCard
         key={poke.name}
         name={poke.name}
         url={poke.url}/>)
 
-    const nextPage = () => {
-        setLimitPage(prevState => prevState + 30)
-        setOffsetPage(prevState => prevState + 30)
-    }
-    const prevPage = () => {
-        setLimitPage(prevState => prevState - 30)
-        setOffsetPage(prevState => prevState - 30)
-    }
+    const nextPage = () => setOffsetPage(prevState => prevState + 30)
+    const prevPage = () => setOffsetPage(prevState => prevState - 30)
+
 
     return (
         <>
