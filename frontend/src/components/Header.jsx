02ea42b530fbe6b5196b296aece8f0bb1f3f61d4
Justin Welsh <justinwelsh33@gmail.com>
2022-10-31 14:42:31 -0500

    new navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,38 @@
+import { HiOutlineMoon } from 'react-icons/hi';
 
 const Header = () => {
 
     return (
-        <div className="navbar p-10 flex justify-center">
-            <h1 className="text-5xl text-black font-bold">
-                Pokedex<span className="text-secondary">.</span>
-            </h1>
+        <div>
+
+            <div className="navbar  p-10">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52">
+                            <li><a>Homepage</a></li>
+                            <li><a>Portfolio</a></li>
+                            <li><a>About</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="navbar-center">
+                    <a className="btn btn-ghost normal-case text-5xl text-black font-bold">Pokedex<span className="text-secondary">.</span></a>
+                </div>
+                <div className="navbar-end">
+                    <button className="btn btn-ghost btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                    </button>
+                    <button className="btn btn-ghost btn-circle">
+                        <div className="indicator">
+                            <HiOutlineMoon className="h-5 w-5" />
+                        </div>
+                    </button>
+                </div>
+            </div>
+
         </div>
     )
 }
